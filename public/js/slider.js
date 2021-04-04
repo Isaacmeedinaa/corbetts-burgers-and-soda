e39b4d3f686a8681e26df290cba61a024a924625
Isaac Medina <isaac.meedinaa@gmail.com>
2021-04-04 02:20:34 -0500

    changes v1.0

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,5 +1,5 @@
 const images = [
-  "/images/bg/hero-bg6-1.jpg",
+  "/images/bg/sodas.png",
   "/images/bg/hero-bg6-2.jpg",
   "/images/bg/hero-bg6-3.jpg",
 ];
@@ -15,9 +15,9 @@ const cycleImages = (images, node, step) => {
 };
 
 const texts = [
-  "Corbett's Burgers and Soda Bar",
-  "The Best Burgers in Town!",
+  "Corbett's Burgers and Soda Bar!",
   "Enjoy Over 250 Craft Bottled Sodas!",
+  "Pour Yourself a Brighter Future, Own a Franchise!",
 ];
 
 const node2 = document.getElementById("frontPageBannerText");
